12d05b7254c470f46faf3368262ac224c0ad7ee4
Hrvoje Šimić <shime@twobucks.co>
2021-12-18 15:49:48 +0100

    Replace tap with jest

--- test.js
+++ test.js
@@ -1,47 +1,40 @@
-var test = require('tape')
-  , time = require('./')
+var time = require('./')
 
-test('it handles most basic stuff', function(t){
-  t.equal(time(2000), '2s', 'time(2000)')
-  t.end()
+test('it handles most basic stuff', function(){
+  expect(time(2000)).toBe('2s')
 })
 
-test('it outputs minutes if present', function(t){
-  t.equal(time(62000), '1m, 2s', 'time(62000)')
-  t.end()
+test('it outputs minutes if present', function(){
+  expect(time(62000)).toBe('1m, 2s')
 })
 
-test('it outputs hours if present', function(t){
-  t.equal(time(3662000), '1h, 1m, 2s', 'time(62000)')
-  t.end()
+test('it outputs hours if present', function(){
+  expect(time(3662000)).toBe('1h, 1m, 2s')
 })
 
-test('it outputs days if present', function(t){
+test('it outputs days if present', function(){
   // 1d = 86400 * 1000ms
   // 1h = 3600 * 1000ms
   // 1m = 60 * 1000ms
   // 1s = 1000ms
-  t.equal(time(90062000), '1d, 1h, 1m, 2s', 'time(90062000)')
-  t.end()
+  expect(time(90062000)).toBe('1d, 1h, 1m, 2s')
 })
 
-test('it outputs months if present', function(t){
+test('it outputs months if present', function(){
   // 1mo = 30 * 86400 * 1000ms
   // 1d = 86400 * 1000ms
   // 1h = 3600 * 1000ms
   // 1m = 60 * 1000ms
   // 1s = 1000ms
-  t.equal(time(2682062000), '1mo, 1d, 1h, 1m, 2s', 'time(2682062000)')
-  t.end()
+  expect(time(2682062000)).toBe('1mo, 1d, 1h, 1m, 2s')
 })
 
-test('it outputs years if present', function(t){
+test('it outputs years if present', function(){
   // 1yr = 12 * 30 * 86400 * 1000ms
   // 1mo = 30 * 86400 * 1000ms
   // 1d = 86400 * 1000ms
   // 1h = 3600 * 1000ms
   // 1m = 60 * 1000ms
   // 1s = 1000ms
-  t.equal(time(33786062000), '1y, 1mo, 1d, 1h, 1m, 2s', 'time(33786062000)')
-  t.end()
+  expect(time(33786062000)).toBe('1y, 1mo, 1d, 1h, 1m, 2s')
 })

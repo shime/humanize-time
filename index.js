e4e158d83841a16719b1f398b29b4435d2059d84
Hrvoje Šimić <shime@twobucks.co>
2021-12-18 15:12:40 +0100

    Add support for days, months and years

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 module.exports = function(ms){
-  var floor = Math.floor
-    , secs  = floor(ms / 1000)
-    , mins  = floor(secs / 60)
-    , hrs   = floor(mins / 60)
+  var floor  = Math.floor
+    , secs   = floor(ms / 1000)
+    , mins   = floor(secs / 60)
+    , hrs    = floor(mins / 60)
+    , days   = floor(hrs / 24)
+    , months = floor(days / 30)
+    , years  = floor(months / 12)
 
   var buff = ""
-  if (hrs > 0){
-    buff = buff.concat(hrs + "h, ")
+
+  if (years > 0) {
+    buff = buff.concat(years, "y, ")
   }
+
+  if (months > 0) {
+    buff = buff.concat(months % 12, "mo, ")
+  }
+
+  if (days > 0) {
+    buff = buff.concat(days % 30, "d, ")
+  }
+
+  if (hrs > 0){
+    buff = buff.concat(hrs % 24 + "h, ")
+  }
+
   if (mins > 0){
     buff = buff.concat(mins % 60 + "m, ")
   }
+
   buff = buff.concat(secs % 60 + "s")
+
   return buff
 }
--- test.js
+++ test.js
@@ -2,16 +2,46 @@ var test = require('tape')
   , time = require('./')
 
 test('it handles most basic stuff', function(t){
-  t.equal(time(2000), '2s', 'time(2000) // 2s')
+  t.equal(time(2000), '2s', 'time(2000)')
   t.end()
 })
 
 test('it outputs minutes if present', function(t){
-  t.equal(time(62000), '1m, 2s', 'time(62000) // 1m, 2s')
+  t.equal(time(62000), '1m, 2s', 'time(62000)')
   t.end()
 })
 
 test('it outputs hours if present', function(t){
-  t.equal(time(3662000), '1h, 1m, 2s', 'time(62000) // 1h, 1m, 2s')
+  t.equal(time(3662000), '1h, 1m, 2s', 'time(62000)')
+  t.end()
+})
+
+test('it outputs days if present', function(t){
+  // 1d = 86400 * 1000ms
+  // 1h = 3600 * 1000ms
+  // 1m = 60 * 1000ms
+  // 1s = 1000ms
+  t.equal(time(90062000), '1d, 1h, 1m, 2s', 'time(90062000)')
+  t.end()
+})
+
+test('it outputs months if present', function(t){
+  // 1mo = 30 * 86400 * 1000ms
+  // 1d = 86400 * 1000ms
+  // 1h = 3600 * 1000ms
+  // 1m = 60 * 1000ms
+  // 1s = 1000ms
+  t.equal(time(2682062000), '1mo, 1d, 1h, 1m, 2s', 'time(2682062000)')
+  t.end()
+})
+
+test('it outputs years if present', function(t){
+  // 1yr = 12 * 30 * 86400 * 1000ms
+  // 1mo = 30 * 86400 * 1000ms
+  // 1d = 86400 * 1000ms
+  // 1h = 3600 * 1000ms
+  // 1m = 60 * 1000ms
+  // 1s = 1000ms
+  t.equal(time(33786062000), '1y, 1mo, 1d, 1h, 1m, 2s', 'time(33786062000)')
   t.end()
 })
